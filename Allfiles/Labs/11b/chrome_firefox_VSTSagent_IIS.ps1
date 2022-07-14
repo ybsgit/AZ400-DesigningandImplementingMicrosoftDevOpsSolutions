2e7bd40fc864aa2239c4e5396f0f95fa872ff763
Yogesh Sangle <44223285+ybsgit@users.noreply.github.com>
2022-07-14 15:53:34 +0530

    Update chrome_firefox_VSTSagent_IIS.ps1

--- Allfiles/Labs/11b/chrome_firefox_VSTSagent_IIS.ps1
+++ Allfiles/Labs/11b/chrome_firefox_VSTSagent_IIS.ps1
@@ -65,7 +65,7 @@ Start-Sleep -s 35
 
 # Remove the installer
 
-rm -Force $workdir\firefox*
+# rm -Force $workdir\firefox*
 
 #copy geckodriver
 Invoke-WebRequest https://github.com/Microsoft/almvm/blob/master/labs/vstsextend/selenium/armtemplate/geckodriver.exe?raw=true -OutFile "C:\Program Files\Mozilla Firefox\geckodriver.exe"
